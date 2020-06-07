3de3251fde2c071a17a3cd5af7358952b4f47a2e
giagia411 <62053377+giagia411@users.noreply.github.com>
2020-06-07 15:05:37 -0700

    Add files via upload

---
+++ 1.4GAugust.py
@@ -0,0 +1,20 @@
+print("Hello World! I wonder why that is always the default coding text to start with")
+
+print(20+5)
+print(20-5)
+print(20*5)
+print(20/5)
+
+orig_string = "Hello"
+full_sentence = orig_string + ", world!"
+print(full_sentence)
+
+genres = ["Sci-Fi", "Horror", "Comedy", "Romantic"]
+print("\nList Items: ")
+print(genres)
+
+genres.append("Action")
+print(genres)
+
+greetings = ("hi", "hello", "hey", "bye")
+print(greetings)
